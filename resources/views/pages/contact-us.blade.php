84106dc04e90d5ff92b0b1f2bc165baec0adbaaf
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2022-11-11 00:21:59 +0500

    QA

--- resources/views/pages/contact-us.blade.php
+++ resources/views/pages/contact-us.blade.php
@@ -7,7 +7,7 @@
         <div class="row">
           <div class="col-lg-12 col-md-12 col-sm-12 col-12">
             <div class="about-banner-content">
-              <h2>Conatact Us</h2>
+              <h2>Contact Us</h2>
             </div>
           </div>
         </div>
