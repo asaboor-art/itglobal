423cbbba65eb7280de4bd5f535c4bcfb6f048203
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2022-12-16 12:13:04 +0500

    footer changes

--- resources/views/footers/footerv2.blade.php
+++ resources/views/footers/footerv2.blade.php
@@ -12,7 +12,7 @@
 						<li><a href="{{ config('site_config.contacts.facebook')}}"><i class="fa fa-facebook"></i></a></li>
 						<!-- <li><a href="{{ config('site_config.contacts.twitter')}}"><i class="fa fa-twitter"></i></a></li> -->
 						<li><a href="{{ config('site_config.contacts.linkedin')}}" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-						<li><a href="{{ config('site_config.contacts.instagram')}}" target="_blank"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="{{ config('site_config.contacts.instragram')}}" target="_blank"><i class="fa fa-instagram"></i></a></li>
 					</ul>
 				</div>
 			</div>
@@ -64,9 +64,9 @@
 			</div>
 			<div class="col-lg-2 col-md-4 col-sm-4 col-12">
 				<div class="qc-links">
-					<h4>Contact Us</h4>
+					<h4>{{ trans('lang.contact')}}</h4>
 					<ul>
-						<li><a href="javascriptvoid:(0)"><i class="fa fa-globe" aria-hidden="true"></i> {{ config('site_config.contacts.website')}}</a></li>
+						<li><a href="{{ route('home') }}"><i class="fa fa-globe" aria-hidden="true"></i> {{ config('site_config.contacts.website')}}</a></li>
 						<li><a href="tel:{{ config('site_config.contacts.phone')}}"><i class="fa fa-phone" aria-hidden="true"></i> {{ config('site_config.contacts.phone')}}</a></li>
 						<li><a href="mailto:{{ config('site_config.contacts.email')}}"><i class="fa fa-envelope" aria-hidden="true"></i> {{ config('site_config.contacts.email')}}</a></li>
 					</ul>
@@ -75,7 +75,7 @@
 		</div>
 		<div class="row k13-link">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-12">
-				<p>© All Copyright 2022 by <a href="javascriptvoid:(0)">K13</a></p>
+			<p class="copy-right-para">{{ config('site_config.contacts.copyright')}}<strong>|</strong>{{ trans('lang.designed_by')}} <a href="javascriptvoid:(0)">{{ config('site_config.contacts.developer')}}</a></p>
 			</div>
 		</div>
 	</div>
