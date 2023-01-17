fe101eeedc10f83af55e58600c8747031c3aebd9
Ghazanfar Ahsan <52095337+GhazanfarAhsan@users.noreply.github.com>
2023-01-17 22:51:14 +0500

    delete branches pages

--- resources/views/homes/indexv1.blade.php
+++ resources/views/homes/indexv1.blade.php
@@ -5,20 +5,9 @@
     @include('sections.bannerv1')
     <!-- BANNER SECTION END -->
     
-    <!-- EXPERIENCE SECTION BEGIN -->
-    @include('sections.experiencev1')
-    <!-- EXPERIENCE SECTION END -->
 
-   
 
-    <!-- INTERIOR & EXTERIOR SECTION BEGIN -->
-    @include('sections.interior-ext')
-    <!-- INTERIOR & EXTERIOR SECTION END -->
-    <!-- WORK WITH US SECTION BEGIN -->
-    @include('sections.work-with-us')
-    <!-- WORK WITH US SECTION END -->
-    <!-- COMMERCIAL SECTION BEGIN -->
-    @include('sections.commercialv1')
+
     <!-- COMMERCIAL SECTION END -->
     <!-- INSTAGRAM FEED SECTION BEGIN -->
     @include('sections.socialv1')
